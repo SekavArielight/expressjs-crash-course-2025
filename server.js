858356cec6edc50c1e6375826ed3ffbed330ed1a
God'swill Sekav Apya <godswillapya@gmail.com>
2025-02-15 17:59:10 +0100

    Add development script and enhance server routes with responses

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express');
+ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {`Server is running on port 3000`});
+app.get('/', (req, res) => {
+    res.send("<h1>Hello Express</h1>");
+});
+
+app.get('/about', (req, res) => {
+    res.send("<h1>About Express</h1>");
+});
+
+app.listen(3000, () => console.log(`Server is running on port 3000`));
