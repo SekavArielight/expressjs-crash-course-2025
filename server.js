893fde532e0f3461fdbc69b309503df5ec49e556
God'swill Sekav Apya <godswillapya@gmail.com>
2025-02-24 15:13:59 +0100

    Enhance API endpoints to support post limit and add single post retrieval

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
- const express = require('express');
- const path = require('path');
- const port = process.env.PORT || 8000;
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -8,13 +8,26 @@ const app = express();
 // app.use(express.static(path.join(__dirname, 'public')));
 
 let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'}
-]
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
 
-app.get('/api/posts', (req, res) => {
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.json(posts.slice(0, limit));
+  } else {
     res.json(posts);
+  }
 });
 
-app.listen(port, () => console.log(`Server is running on port ${port}`)); 
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
