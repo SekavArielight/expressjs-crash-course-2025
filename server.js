330a732e988184d6c0704492f8c097703e15c991
God'swill Sekav Apya <godswillapya@gmail.com>
2025-03-01 23:40:22 +0100

    Add logger middleware for request logging and update server configuration

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const path = require("path");
 const posts = require('./routes/posts');
+const logger = require("./middleware/logger.middleware");
 const port = process.env.PORT || 8000;
 
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
 
 // Setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
